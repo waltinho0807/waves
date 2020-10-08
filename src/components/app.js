7e2e0977edb1de3df239598901aff0cda7532a75
waltinhocalegari <waltinhocalegari@hotmail.com>
2020-10-08 13:38:31 -0300

    prod

--- src/components/app.js
+++ src/components/app.js
@@ -1,37 +1,37 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
+import Waves from "@waves/signer";
+import Provider from "@waves.exchange/provider-web";
 
-class App extends React.Component{
-    constructor (props){
+class App extends React.Component {
+    constructor(props) {
         super(props);
         this.state = {};
         this.authFunc = this.authFunc.bind(this);
     }
 
-    authFunc(){
-        const authData = {data: "auth my site"};
-        if(WavesKeeper){
-            WavesKeeper.auth(authData)
-            .then(auth => {
-                console.log(auth);
-            }).catch(error => {
-                console.log(error)
-            });
-        }else {
-            alert("Install waves Keeper")
-        }
+    async authFunc() {
+        // setting the TestNet Signer
+        const waves = new Waves({ NODE_URL: 'https://pool.testnet.wavesnodes.com' });
+        // setting the Waves.Exchange provider
+        const provider = new Provider('https://testnet.waves.exchange/signer/');
+        waves.setProvider(provider);
+        const userData = await waves.login();
+        return userData;
+        console.log(userData)
     }
+    
 
-    render(){
-        return(
+    render() {
+        return (
             <div className="container">
-                <input className="btn btn-primary" type="submit" value="Alert" onClick={this.authFunc}/>
+                <input className="btn btn-primary" type="submit" value="Alert" onClick={this.authFunc} />
             </div>
         )
     }
 }
 
 const app = document.getElementById('app');
-if(app){
-    ReactDOM.render(<App/>, app);
+if (app) {
+    ReactDOM.render(<App />, app);
 }
